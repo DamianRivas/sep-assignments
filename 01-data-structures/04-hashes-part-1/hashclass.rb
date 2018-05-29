b8a0b082785836898c1868961e9bb10092a5cca4
damianrivas <drivas1993@gmail.com>
2018-05-28 22:16:13 -0700

    Create a hash data structure

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -1,3 +1,5 @@
+require_relative 'hash_item'
+
 class HashClass
 
   def initialize(size)
@@ -5,23 +7,58 @@ class HashClass
   end
 
   def []=(key, value)
+    i = index(key, size)
+
+    if !@items[i]
+      @items[i] = HashItem.new(key, value)
+    elsif @items[i].key == key
+      @items[i].value = value
+    else
+      resize
+      self[key] = value
+    end
   end
 
 
   def [](key)
+    i = index(key, size)
+
+    if @items[i]
+      @items[i].value
+    end
   end
 
   def resize
+    newArr = Array.new(size * 2)
+
+    @items.each do |item|
+      if item
+        i = index(item.key, newArr.size)
+
+        newArr[i] = item
+      end
+    end
+
+    @items = newArr
+
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    sum = 0
+
+    key.each_byte do |c|
+      sum += c
+    end
+
+    sum % size
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @items.size
   end
 
 end
